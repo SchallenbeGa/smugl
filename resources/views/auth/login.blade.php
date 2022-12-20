b9ed54458f20c04e2e348d92b75f5fe67464689b
Gabriel <admin@gabriel0.com>
2022-12-20 12:21:50 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,6 @@
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
-        <p>pull automatically</p>
         <form method="POST" action="{{route('login')}}">
             @csrf
 
